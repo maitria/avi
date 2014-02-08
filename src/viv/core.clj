139a671e73119fe34ac4e58d9daf67289b570cea
Jason Felice <jason.m.felice@gmail.com>
2014-02-08 00:40:29 -0500

    Fix initially broken test

--- src/viv/core.clj
+++ src/viv/core.clj
@@ -1,6 +1,1 @@
 (ns viv.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/viv/core_test.clj
+++ test/viv/core_test.clj
@@ -1,7 +1,3 @@
 (ns viv.core-test
   (:require [clojure.test :refer :all]
             [viv.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
