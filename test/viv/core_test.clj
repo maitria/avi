b808350487c72e65f25bed42463edaf14d7eb362
Jason Felice <jason.m.felice@gmail.com>
2014-02-08 22:45:21 -0500

    Don't depend on the content of README

--- test/viv/core_test.clj
+++ test/viv/core_test.clj
@@ -8,6 +8,7 @@
     (= string (.substring (core/screen-line viv i) j (+ j (count string))))))
 
 (fact "it displays the content of loaded files"
-  (let [viv (core/start [80 25] ["README.md"])]
-    viv => (displays "# viv" :at [0 0])
-    viv => (displays "## Usage" :at [4 0])))
+  (let [viv (core/start [80 25] ["test/test.txt"])]
+    viv => (displays "One" :at [0 0])
+    viv => (displays "Two" :at [1 0])
+    viv => (displays "Three" :at [2 0])))
