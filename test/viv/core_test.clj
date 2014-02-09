23f4a54ea7afd828ad7257d87c628bd91dfbffb5
Jason Felice <jason.m.felice@gmail.com>
2014-02-08 23:21:32 -0500

    Group facts about loading a file on start

--- test/viv/core_test.clj
+++ test/viv/core_test.clj
@@ -9,17 +9,18 @@
           actual (.substring line j (+ j (count expected)))]
       (= expected actual))))
 
-(fact "it displays the content of loaded files"
-  (let [viv (core/start [10 80] ["test/test.txt"])]
-    viv => (displays "One" :at [0 0])
-    viv => (displays "Two" :at [1 0])
-    viv => (displays "Three" :at [2 0])))
+(facts "about loading a file on start"
+  (fact "it displays the content of loaded files"
+    (let [viv (core/start [10 80] ["test/test.txt"])]
+      viv => (displays "One" :at [0 0])
+      viv => (displays "Two" :at [1 0])
+      viv => (displays "Three" :at [2 0])))
 
-(fact "it displays tildes after the file contents"
-  (let [viv (core/start [10 80] ["test/test.txt"])]
-    viv => (displays "~" :at [3 0])
-    viv => (displays "~" :at [7 0])))
+  (fact "it displays tildes after the file contents"
+    (let [viv (core/start [10 80] ["test/test.txt"])]
+      viv => (displays "~" :at [3 0])
+      viv => (displays "~" :at [7 0])))
 
-(fact "it displays the filename in the status bar"
-  (let [viv (core/start [10 80] ["test/test.txt"])]
-    viv => (displays "test/test.txt" :at [8 0])))
+  (fact "it displays the filename in the status bar"
+    (let [viv (core/start [10 80] ["test/test.txt"])]
+      viv => (displays "test/test.txt" :at [8 0]))))
