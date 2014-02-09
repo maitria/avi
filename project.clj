e019a0a6cbf793aceedb500f5c6603ca2c8cb0d7
Jason Felice <jason.m.felice@gmail.com>
2014-02-08 19:46:09 -0500

    Don't bounce a dock icon when booting

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :jvm-opts ["-Dapple.awt.UIElement=true"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clojure-lanterna "0.9.4"]]
   :main viv.core)
