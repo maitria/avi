174f90fc127b0e582f75886362e6584ca8354156
Jason Felice <jason.m.felice@gmail.com>
2014-02-24 20:26:05 -0600

    Allow omitting attributes on looks-like lines

--- test/avi/core_test.clj
+++ test/avi/core_test.clj
@@ -3,17 +3,15 @@
             [avi.test-helpers :refer :all]))
 
 (facts "regarding displaying of a loaded file"
-  (fact "Each line is displayed on a different line."
-    (editor) => (renders "One            " :at [0 0])
-    (editor) => (renders "Two            " :at [1 0])
-    (editor) => (renders "Three          " :at [2 0]))
-  (fact "Tildes are displayed on blank lines."
-    (editor) => (renders "~              " :at [4 0] :in :blue)
-    (editor) => (renders "~              " :at [7 0] :in :blue))
-  (fact "The filename appears in the status bar."
-    (editor) => (renders "test/test.txt  " :at [8 0] :in :black :on :white))
-  (fact "The prompt line is clear."
-    (editor) => (renders "               " :at [9 0])))
+  (editor) => (looks-like
+                "One            "
+                "Two            "
+                "Three          "
+                ".              "
+                "~              " :blue
+                "~              " :blue
+                "test/test.txt  " :inverse
+                "               "))
 
 (facts "regarding quitting"
   (fact "It doesn't start in the 'finished' state."
--- test/avi/test_helpers.clj
+++ test/avi/test_helpers.clj
@@ -2,25 +2,52 @@
   (:require [avi.core :as core]
             [avi.render :as render]))
 
-(defn renders
-  [expected & {[i j] :at,
-               expected-color :in,
-               expected-background :on,
-               :or {expected-color :white,
-                    expected-background :black}}]
+(defn- text-matches-rendering?
+  [rendering i text]
+  (let [{:keys [chars width]} rendering 
+        actual-text (String. chars (* i width) width)]
+    (= text actual-text)))
+
+(defn- color-matches-rendering?
+  [rendering i color]
+  (let [{:keys [attrs width]} rendering
+        offset (* i width)
+        line-attrs (subvec (into [] attrs) offset (+ offset width))
+        expected-attrs (case color
+                         :white (render/make-attrs :white :black)
+                         :blue (render/make-attrs :blue :black)
+                         :inverse (render/make-attrs :black :white))]
+    (->> line-attrs
+         (filter (partial not= color))
+         seq)))
+
+(defn- partition-lines
+  [lines]
+  (loop [[text maybe-attributes & lines] lines
+         result []]
+    (cond
+      (not text)
+      result
+
+      (keyword? maybe-attributes)
+      (recur lines
+             (conj result [text maybe-attributes]))
+
+      :else
+      (recur (cons maybe-attributes lines)
+             (conj result [text :white])))))
+
+(defn looks-like
+  [& args]
   (fn [editor]
-    (let [{:keys [chars attrs width]} (render/render editor)
-          expected-attrs (render/make-attrs expected-color expected-background)
-          expected-chars (->> (range (count expected))
-                              (map (partial + j))
-                              (map #(vector %1 i %2) expected))]
-      (every?
-        (fn [[c i j]]
-          (let [index (+ j (* i width))]
-            (and
-              (= (get chars index) c)
-              (= (get attrs index) expected-attrs))))
-        expected-chars))))
+    (let [rendering (render/render editor)]
+      (->> args
+           partition-lines
+           (map-indexed 
+             (fn [i [text color]]
+               (and (text-matches-rendering? rendering i text)
+                    (color-matches-rendering? rendering i color))))
+           (every? identity)))))
 
 (defn editor
   [& {file-contents :when-editing,
@@ -32,7 +59,7 @@
         events (concat key-events (if event
                                     [event]))
         initial-editor (with-redefs [slurp (constantly file-contents)]
-                         (core/start [10 15] "test/test.txt"))]
+                         (core/start [8 15] "test/test.txt"))]
     (reduce
       core/process
       initial-editor
