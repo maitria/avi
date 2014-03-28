8c58896a225deccba899a45f7e4f61fcec4e0076
Jason Felice <jason.m.felice@gmail.com>
2014-03-28 11:21:18 -0400

    Clojure 1.6.0, midje 1.6.3

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://github.com/maitria/avi/README.md"}
   :jvm-opts ["-Djava.library.path=/usr/local/lib/avi"]
   :plugins [[lein-midje "3.0.0"]]
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}
              :uberjar {:aot :all}}
   :aliases {"install" ^{:doc "Install avi on this machine"}
             ["do" "uberjar" ["run" "-m" "avi.install/install"]]}
