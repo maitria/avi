28541502dcc476640b104c4d46750110c056d2c9
Jason Felice <jason.m.felice@gmail.com>
2014-04-30 15:14:41 -0500

    Use packthread 0.1.1

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :jvm-opts ["-Djava.library.path=/usr/local/lib/avi"]
   :plugins [[lein-midje "3.0.0"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.maitria/packthread "0.1.0"]]
+                 [com.maitria/packthread "0.1.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}
              :uberjar {:aot :all}}
   :aliases {"install" ^{:doc "Install avi on this machine"}
