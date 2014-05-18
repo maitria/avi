cf48fbb73c9fa2636172db5552dbce2747ce0524
Jason Felice <jason.m.felice@gmail.com>
2014-05-17 20:27:14 -0400

    Rename `line` to `string`

--- src/avi/string.clj
+++ src/avi/string.clj
@@ -1,10 +1,10 @@
 (ns avi.string)
 
 (defn index-of-first-non-blank
-  [line]
-  (let [leading-space-count (count (re-find #"^\s*" line))
+  [string]
+  (let [leading-space-count (count (re-find #"^\s*" string))
         all-spaces? (and (> leading-space-count 0)
-                         (= leading-space-count (count line)))]
+                         (= leading-space-count (count string)))]
     (if all-spaces?
       (dec leading-space-count)
       leading-space-count)))
