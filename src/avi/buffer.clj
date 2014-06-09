9d3ec03a4d2dceaa16e5128225c9cac32d35660f
Jason Felice <jason.m.felice@gmail.com>
2014-06-09 12:10:51 -0400

    Update buffer contents in both places

--- src/avi/buffer.clj
+++ src/avi/buffer.clj
@@ -26,6 +26,12 @@
      :last-explicit-j 0
      :changes (list {:lines lines})}))
 
+(defn- update-lines
+  [buffer lines]
+  (+> buffer
+      (assoc :lines lines)
+      (assoc :changes (list {:lines lines}))))
+
 (defn write
   [{lines :lines,
     filename :name}]
@@ -171,8 +177,8 @@
 ;; -- changing buffer contents --
 
 (defn- update-line
-  [buffer i modify-fn]
-  (update-in buffer [:lines i] modify-fn))
+  [{lines :lines, :as buffer} i modify-fn]
+  (update-lines buffer (update-in lines [i] modify-fn)))
 
 (defn insert-text
   [{[i j] :cursor,
@@ -186,7 +192,7 @@
             resulting-j (if (= 1 (count new-lines))
                           (+ j (count text))
                           0)]
-        (assoc :lines (splice lines i (inc i) new-lines))
+        (update-lines (splice lines i (inc i) new-lines))
         (move-cursor [resulting-i resulting-j] resulting-j))))
 
 (defn insert-blank-line
@@ -194,7 +200,7 @@
     lines :lines,
     :as buffer} new-line-i]
   (+> buffer
-      (assoc :lines (splice lines new-line-i new-line-i [""]))))
+      (update-lines (splice lines new-line-i new-line-i [""]))))
 
 (defn delete-char-under-cursor
   [{[i j] :cursor,
@@ -209,7 +215,7 @@
   (+> buffer
       (if (= 1 (line-count buffer))
         (do
-          (assoc :lines [""])
+          (update-lines [""])
           (move-cursor [0 0] 0))
         (let [new-lines (splice lines i (inc i))
               new-i (if (= i (dec (line-count buffer)))
@@ -217,7 +223,7 @@
                       i)
               target-line (get new-lines new-i)
               new-j (s/index-of-first-non-blank target-line)]
-          (assoc :lines new-lines)
+          (update-lines new-lines)
           (move-cursor [new-i new-j])))))
 
 (defn- backspace-at-beginning-of-line
@@ -230,7 +236,7 @@
             i (dec i)
             j (count (get lines i))]
         (move-cursor [i j] j)
-        (assoc :lines new-lines))))
+        (update-lines new-lines))))
 
 (defn backspace
   [{[i j] :cursor,
