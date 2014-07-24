a5be78fd14dd6d5d1b290735f6ee953f0a2a6ff7
Angela Harms <angela.harms@gmail.com>
2014-07-24 19:36:48 -0400

    Remove unneeded alias

--- src/avi/pervasive.clj
+++ src/avi/pervasive.clj
@@ -1,5 +1,5 @@
 (ns avi.pervasive
-  (:require [avi.assert :as test])) 
+  (:require [avi.assert])) 
 (defn splice
   "Replace or delete elements starting at the start index, up to but not
   including the end index.
