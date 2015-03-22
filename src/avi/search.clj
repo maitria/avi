b66c893288a7bce9597ba668f8c33069c8d30ef9
Jason Felice <jason.m.felice@gmail.com>
2015-03-21 22:27:17 -0400

    Simplify `process-search`

--- src/avi/search.clj
+++ src/avi/search.clj
@@ -9,21 +9,21 @@
   (e/keystroke-middleware "/" #(cl/enter % :forward-search "/")))
 
 (defn find-next
-  [lines [i j] re]
-  (if (>= i (count lines))
-    nil
-    (let [m (re-matcher re (get lines i))]
-      (if (.find m j)
-        [i (.start m)]
-        (recur lines [(inc i) 0] re)))))
+  ([{:keys [lines [i j] :cursor]} re]
+   (find-next lines [i j] re))
+  ([lines [i j] re]
+   (if (>= i (count lines))
+     nil
+     (let [m (re-matcher re (get lines i))]
+       (if (.find m j)
+         [i (.start m)]
+         (recur lines [(inc i) 0] re))))))
 
 (defn process-search
   [editor command-line]
   (+> editor
-    (let [{:keys [lines] [i j] :cursor} (e/current-buffer editor)
-          found-pos (find-next lines [i j] (re-pattern command-line))]
-      (if found-pos
-        (in e/current-buffer (b/move-cursor found-pos (second found-pos)))
-        (assoc :message [:white :red (str "Did not find `" command-line "`.")])))))
+    (if-let [[i j] (find-next (e/current-buffer editor) (re-pattern command-line))]
+       (in e/current-buffer (b/move-cursor [i j] j))
+       (assoc :message [:white :red (str "Did not find `" command-line "`.")]))))
 
 (def wrap-mode (cl/mode-middleware :forward-search process-search))
