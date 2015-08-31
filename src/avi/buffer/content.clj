54c1761a612be752ea7cf1b7d7703b61e8f3e620
Jason Felice <jason.m.felice@gmail.com>
2015-08-31 16:59:21 -0400

    replace can insert at beginning of buffer

--- src/avi/buffer/content.clj
+++ src/avi/buffer/content.clj
@@ -1,10 +1,30 @@
 (ns avi.buffer.content
+  (:refer-clojure :exclude [replace])
   (:require [clojure.string :as string]
             [schema.core :as s]))
 
+(def Line (s/both s/Int (s/pred pos?)))
+(def Column (s/both s/Int (s/pred (complement neg?))))
+
+(def Mark
+  [(s/one Line "Line") 
+   (s/one Column "Column")])
+
 (def Content
-  {:lines [s/Str]})
+  {:lines [(s/one s/Str "first line") s/Str]})
 
 (s/defn content :- Content
   [text :- s/Str]
   {:lines (string/split text #"\n")})
+
+(s/defn replace :- Content
+  "Replace text between the `start` mark and the `end` mark with `replacement`.
+
+  `replacement` may contain newlines, and the `start` and `end` marks can span
+  lines; therefore, this is the most general content operation which can insert,
+  delete, or replace text."
+  [content :- Content
+   start :- Mark
+   end :- Mark
+   replacement :- s/Str]
+  (update-in content [:lines 0] #(str replacement %)))
--- test/avi/buffer/content_test.clj
+++ test/avi/buffer/content_test.clj
@@ -1,10 +1,17 @@
 (ns avi.buffer.content-test
   (:require [midje.sweet :refer :all]
             [schema.core :as s]
-            [avi.buffer.content :as content]))
+            [avi.buffer.content :as c]))
 
 (s/set-fn-validation! true)
 
 (facts "about buffer contents"
-  (fact "we can retrieve its initial text"
-    (:lines (content/content "Hello, World!")) => ["Hello, World!"]))
+  (fact "we can retrieve buffer contents initial text"
+    (:lines (c/content "Hello, World!")) => ["Hello, World!"]
+    (:lines (c/content "Line 1\nLine 2")) => ["Line 1" "Line 2"]
+    (:lines (c/content "Line 1\nLine 3\n")) => ["Line 1" "Line 3"]
+    (:lines (c/content "Line 1\n\nLine 3")) => ["Line 1" "" "Line 3"]))
+
+(facts "about replacing contents"
+  (fact "replace can insert at beginning of buffer"
+    (:lines (c/replace (c/content "Hello!") [1 0] [1 0] "xyz")) => ["xyzHello!"]))
