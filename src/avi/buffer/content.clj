bc0edb13ed1c8f600857d3a768602994a166bcb5
Jason Felice <jason.m.felice@gmail.com>
2015-08-31 21:58:24 -0500

    alot

--- src/avi/buffer/content.clj
+++ src/avi/buffer/content.clj
@@ -37,17 +37,56 @@
   [text :- s/Str]
   {:lines (split-lines text)})
 
+(s/defn lines-upto :- [Line]
+  [lines :- [Line]
+   [end-line end-column] :- Mark]
+  (-> lines
+    (subvec 0 (dec end-line))
+    (conj (subs (get lines (dec end-line)) 0 end-column))))
+
+(s/defn lines-from :- [Line]
+  [lines :- [Line]
+   [start-line start-column] :- Mark]
+  (vec (concat [(subs (get lines (dec start-line)) start-column)]
+               (subvec lines start-line))))
+
+(defn join-line-sets
+  ([a b]
+   (vec (concat (drop-last a)
+                [(str (last a) (first b))]
+                (drop 1 b))))
+  ([a b c]
+   (join-line-sets (join-line-sets a b) c)))
+
+(defn- replacement-lines
+  [text]
+  (let [stopping-point (count text)]
+    (loop [line-start 0
+           current-offset 0
+           lines []]
+      (cond
+        (= current-offset stopping-point)
+        (conj lines (subs text line-start current-offset))
+
+        (= (get text current-offset) \newline)
+        (recur (inc current-offset)
+               (inc current-offset)
+               (conj lines (subs text line-start current-offset)))
+
+        :else
+        (recur line-start (inc current-offset) lines)))))
+
 (s/defn replace :- Content
   "Replace text between the `start` mark and the `end` mark with `replacement`.
 
   `replacement` may contain newlines, and the `start` and `end` marks can span
   lines; therefore, this is the most general content operation which can insert,
   delete, or replace text."
-  [content :- Content
-   [start-line start-column] :- Mark
+  [{:keys [lines] :as content} :- Content
+   start :- Mark
    end :- Mark
    replacement :- s/Str]
-  (update-in content [:lines (dec start-line)] #(str
-                                            (subs % 0 start-column)
-                                            replacement
-                                            (subs % start-column))))
+  (assoc content :lines (join-line-sets
+                          (lines-upto lines start)
+                          (replacement-lines replacement)
+                          (lines-from lines end))))
