d47498d002558cb3ec46afb3b8397bb4bd1ebc16
Jason Felice <jason.m.felice@gmail.com>
2015-10-20 16:12:28 -0400

    `after` a location after end-of-file has only an empty line

--- test/avi/buffer/t_lines.clj
+++ test/avi/buffer/t_lines.clj
@@ -59,7 +59,7 @@
     (lines/before ["x"] [2 2]) => ["x" "" "  "])
   (fact "`after` a location after end-of-line keeps the newline"
     (lines/after ["x"] [0 4]) => [""])
-  (fact "`after` a location after end-of-file"
+  (fact "`after` a location after end-of-file has only an empty line"
     (lines/after ["x"] [5 0]) => [""])
   (fact "can replace a line after the last"
     (lines/replace (lines/content "") [1 0] [1 0] "xyz") => ["" "xyz"])
