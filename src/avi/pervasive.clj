d82d8f19970b37409c9fe216d198bf63727a1718
Jason Felice <jason.m.felice@gmail.com>
2015-10-20 16:04:39 -0400

    `:pre` and `:post` for `subs-with-spaces`

--- src/avi/pervasive.clj
+++ src/avi/pervasive.clj
@@ -66,11 +66,13 @@
   string.  If `start` is greater, we pretend that the string was longer.  If
   `end` is greater, we pretend as theough the string were padded with spaces."
   ([s start]
+   {:pre (string? s)}
    (if (> start (count s))
      ""
      (subs s start)))
   ([s start end]
-   {:post [(= (count %) (- end start))]}
+   {:pre [(string? s)]
+    :post [(= (count %) (- end start))]}
    (let [s-start (min start (count s))
          s-end (min end (count s))]
      (apply str
