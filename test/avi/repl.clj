6d275b5b91c8f931845beb55d1ac6c4a00d13e5e
Jason Felice <jason.m.felice@gmail.com>
2015-10-20 16:04:04 -0400

    Load avi.pervasive in the repl

--- test/avi/repl.clj
+++ test/avi/repl.clj
@@ -1,4 +1,5 @@
 (ns avi.repl
   "This namespace is for utilities used during development, including testing and so forth."
-  (:require [avi.test-helpers :refer :all]
+  (:require [avi.pervasive :refer :all]
+            [avi.test-helpers :refer :all]
             [midje.repl :refer :all]))
