91b5adb0c7468ab8a3f98693df4a779d3b5b0860
Jason Felice <jason.m.felice@gmail.com>
2015-11-10 21:01:51 -0500

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
             :url "http://github.com/maitria/avi/README.md"}
   :jvm-opts ["-Djava.library.path=/usr/local/lib/avi"]
   :plugins [[lein-midje "3.0.0"]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.maitria/packthread "0.1.6"]
                  [prismatic/schema "1.0.2"]
                  [potemkin "0.4.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]
+  :profiles {:dev {:dependencies [[midje "1.7.0"]
                                   [org.clojure/test.check "0.7.0"]
-                                  [com.gfredericks/test.chuck "0.1.15"]]}
+                                  [com.gfredericks/test.chuck "0.1.18"]]}
              :uberjar {:aot :all}}
   :repl-options {:init-ns avi.repl}
   :aliases {"install" ^{:doc "Install avi on this machine"}
