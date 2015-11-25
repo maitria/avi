7f4d9136df1d114a8e80c837cdfeb13458497fe8
Jason Felice <jason.m.felice@gmail.com>
2015-11-25 15:06:00 -0500

    Formatting

--- test/avi/t_normal_mode.clj
+++ test/avi/t_normal_mode.clj
@@ -423,13 +423,12 @@
       (editor :editing ?text :after (str "ll" ?keys)) => (every-pred
                                                            (line 0 ?line)
                                                            (point ?pos)))
-    ?text    ?keys ?pos  ?line
-    "abcd)f" "df)" [0 2] "abf"
-    "abcd/f" "df/" [0 2] "abf"
-    "abcdef" "dl"  [0 2] "abdef"
-    "abcdef" "dh"  [0 1] "acdef"
-    "a\nb\nc" "dj" [0 0] "c"
-    )
+    ?text     ?keys ?pos  ?line
+    "abcd)f"  "df)" [0 2] "abf"
+    "abcd/f"  "df/" [0 2] "abf"
+    "abcdef"  "dl"  [0 2] "abdef"
+    "abcdef"  "dh"  [0 1] "acdef"
+    "a\nb\nc" "dj" [0 0] "c")
   (fact "`df)` beeps if there's no `)`"
     (editor :editing "y12x" :after "ldf)") => (line 0 "y12x")
     (editor :editing "y12x" :after "ldf)") => beeped)
