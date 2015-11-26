52c4e8561ef69705b093d8cae62d481d1329de8c
Jason Felice <jason.m.felice@gmail.com>
2015-11-25 20:25:55 -0500

    Extract `adjust-for-motion-kind`

--- src/avi/buffer/motion.clj
+++ src/avi/buffer/motion.clj
@@ -36,27 +36,44 @@
         clamp-point-j
         c/adjust-viewport-to-contain-point))))
 
+(defmulti adjust-for-motion-kind
+  "Per vim docs, \"inclusive\" motions include the last character of the range
+  (which we exclude by default because of the way we track the point).
+  \"Exclusive\" motions do what we normally do, and \"linewise\" motions
+  affect all lines they touch.
+
+  Linewise gets difficult: we want to take either a preceeding _or_ a trailing
+  newline with the change or deletion _if we have one_."
+  (fn [range lines kind] kind))
+
+(defmethod adjust-for-motion-kind :exclusive
+  [range _ _]
+  range)
+
+(defmethod adjust-for-motion-kind :inclusive
+  [[start [ei ej]] _ _]
+  [start [ei (inc ej)]])
+
+(defmethod adjust-for-motion-kind :linewise
+  [[[si sj] [ei ej]] lines _]
+  (cond
+    (and (not (get lines (dec si)))
+         (not (get lines (inc ei))))
+    [[0 0] [(dec (count lines)) (count (peek lines))]]
+
+    (get lines (inc ei))
+    [[si 0] [(inc ei) 0]]
+
+    :else
+    [[(dec si) (count (get lines (dec si)))]
+     [ei (count (get lines ei))]]))
+
 (defn resolve-range
   [{:keys [lines point] :as buffer} motion kind]
   (when-let [pos (resolve/resolve-motion buffer motion)]
-    (let [[start end] (sort [point pos])
-          [start end] (case kind
-                        :exclusive [start end]
-                        :inclusive [start [(first end) (inc (second end))]]
-                        :linewise  (let [[si sj] start
-                                         [ei ej] end]
-                                     (cond
-                                       (and (not (get lines (dec si)))
-                                            (not (get lines (inc ei))))
-                                       [[0 0] [(dec (count lines)) (count (peek lines))]]
-
-                                       (get lines (inc ei))
-                                       [[si 0] [(inc ei) 0]]
-
-                                       :else
-                                       [[(dec si) (count (get lines (dec si)))]
-                                        [ei (count (get lines ei))]])))]
-      [start end])))
+    (-> [point pos]
+      sort
+      (adjust-for-motion-kind lines kind))))
 
 (defn delete
   [{start :point :keys [lines] :as buffer} motion kind]
