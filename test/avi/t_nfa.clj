40ff0827d01b2ca13409fc6a5f08b4a1c0d34fec
Jason Felice <jason.m.felice@gmail.com>
2015-11-27 14:21:44 -0500

    Extract `accepts`

--- test/avi/t_nfa.clj
+++ test/avi/t_nfa.clj
@@ -2,11 +2,18 @@
   (:require [avi.nfa :refer :all]
             [midje.sweet :refer :all]))
 
+(defn accepts
+  [nfa inputs]
+  (accept? nfa (reduce
+                 (fn [state input]
+                   (if (= state :reject)
+                     :reject
+                     (advance nfa state input :reject)))
+                 (start nfa)
+                 inputs)))
+
 (facts "about match nfa"
   (fact "it doesn't start in an accept state"
-    (let [nfa (match "1")]
-      (accept? nfa (start nfa))) => falsey)
+    (accepts (match "1") []) => falsey)
   (fact "it accepts the supplied value"
-    (let [nfa (match "1")
-          adv #(advance nfa %1 %2 :reject)]
-      (accept? nfa (-> (start nfa) (adv "1"))) => truthy)))
+    (accepts (match "1") ["1"]) => truthy))
