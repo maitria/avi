920adad5475301a1b27c5ebf4d1d5f49feccacf1
Jason Felice <jason.m.felice@gmail.com>
2015-11-27 14:44:30 -0500

    Extract helper fns for testing NFAs

--- test/avi/t_nfa.clj
+++ test/avi/t_nfa.clj
@@ -2,38 +2,41 @@
   (:require [avi.nfa :refer :all]
             [midje.sweet :refer :all]))
 
+(defn- state-after-inputs
+  [nfa inputs]
+  (reduce
+    (fn [s input]
+      (if (= :reject s)
+        :reject
+        (advance nfa s input :reject)))
+    (start nfa)
+    inputs))
+
+(defn- characterize-state
+  [nfa state]
+  (cond
+    (= :reject state)   :reject
+    (accept? nfa state) :accept
+    :else               :pending))
+
 (tabular
   (facts "about NFAs"
     (let [nfa ?nfa
-          final-state (reduce
-                        (fn [s input]
-                          (if (= :reject s)
-                            :reject
-                            (advance nfa s input :reject)))
-                        (start nfa)
-                        ?inputs)
-          result (cond
-                   (= :reject final-state)
-                   :reject
-
-                   (accept? nfa final-state)
-                   :accept
-
-                   :else
-                   nil)]
+          state (state-after-inputs nfa ?inputs)
+          result (characterize-state nfa state)]
       result => ?result))
 
-  ?nfa              ?inputs   ?result
-  (match "1")       []        nil   
-  (match "1")       ["1"]     :accept
-  (match "1")       ["2"]     :reject
-  (match "1")       ["1" "2"] :reject
-  (any)             []        nil
-  (any)             ["1"]     :accept
-  (any)             ["2"]     :accept
-  (any)             ["1" "2"] :reject
-  (opt (match "1")) []        :accept
-  (opt (match "1")) ["1"]     :accept
-  (opt (match "1")) ["2"]     :reject
-  (opt (match "1")) ["1" "1"] :reject
-  (opt (match "1")) ["1" "2"] :reject)
+  ?nfa             ?inputs  ?result
+  (match 1)        []       :pending
+  (match 1)        [1]      :accept
+  (match 1)        [2]      :reject
+  (match 1)        [1 2]    :reject
+  (any)            []       :pending
+  (any)            [1]      :accept
+  (any)            [2]      :accept
+  (any)            [1 2]    :reject
+  (opt (match 1))  []       :accept
+  (opt (match 1))  [1]      :accept
+  (opt (match 1))  [2]      :reject
+  (opt (match 1))  [1 1]    :reject
+  (opt (match 1))  [1 2]    :reject)
