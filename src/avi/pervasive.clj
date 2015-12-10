4fbc36bc8b9d6b9955a06269389a8c217a16d4df
Jason Felice <jason.m.felice@gmail.com>
2015-12-09 23:34:57 -0500

    Remove unused `splice`

--- src/avi/pervasive.clj
+++ src/avi/pervasive.clj
@@ -1,43 +1,5 @@
 (ns avi.pervasive)
 
-(defn- subcollection
-  [collection & rest-of-args]
-  (if (string? collection)
-    (apply subs collection rest-of-args)
-    (apply subvec collection rest-of-args)))
-
-(def ^:private vector-concatenate
-  (comp vec concat))
-
-(defn- concatenate 
-  [& collections]  
-  (if (string? (first collections))
-    (apply str collections)
-    (apply vector-concatenate collections)))
-
-(defn- splice-head
-  [collection splice-start]
-  (subcollection collection 0 splice-start))
-
-(defn- splice-tail
-  [collection splice-end]
-  (if (>= splice-end (count collection))
-    (empty collection)
-    (subcollection collection splice-end)))
-
-(defn splice
-  "Replace or delete elements starting at the splice-start, up to but not
-  including splice-end.
-
-  It is not an error for end to be well past the end of the collection."
-  ([collection splice-start splice-end]
-   (splice collection splice-start splice-end (empty collection)))
-  ([collection splice-start splice-end replacements]
-   (concatenate
-     (splice-head collection splice-start)
-     replacements
-     (splice-tail collection splice-end))))
-
 (defn n-times
   [thing n a-fn]
   (reduce
