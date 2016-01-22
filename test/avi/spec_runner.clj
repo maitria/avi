877bde4b949e85cfc0247c4fa9a5ca36a273a34c
Jason Felice <jason.m.felice@gmail.com>
2016-01-21 22:29:49 -0500

    `facts-about` takes plain column names

--- test/avi/spec_runner.clj
+++ test/avi/spec_runner.clj
@@ -2,10 +2,15 @@
   (:require [midje.sweet :refer :all])
   (:require [avi.test-helpers :refer :all]))
 
+(def ^:private column-names
+  '{content ?content
+    after   ?after
+    point   ?point})
+
 (defmacro facts-about
   [description columns & args]
   `(tabular
      (facts ~description
        (editor :editing ~'?content :after ~'?after) => (point ~'?point))
-     ~@columns
+     ~@(map column-names columns)
      ~@args))
