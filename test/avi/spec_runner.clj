13122df33b6c49e74530fec12ca6a997d93d1a7d
Jason Felice <jason.m.felice@gmail.com>
2016-01-21 22:15:42 -0500

    `facts-about` column names go in a vector

--- test/avi/spec_runner.clj
+++ test/avi/spec_runner.clj
@@ -3,8 +3,9 @@
   (:require [avi.test-helpers :refer :all]))
 
 (defmacro facts-about
-  [description & args]
+  [description columns & args]
   `(tabular
      (facts ~description
        (editor :editing ~'?content :after ~'?after) => (point ~'?point))
+     ~@columns
      ~@args))
