b8ee54fd2aa740b0aff0094ad5a5b05a6f2a106f
Alex Harms <onealexharms@gmail.com>
2016-04-14 09:20:56 -0700

    Clarify tests.

--- test/avi/t_spec_runner.clj
+++ test/avi/t_spec_runner.clj
@@ -2,15 +2,16 @@
   (:require [midje.sweet :refer :all]
             [avi.spec-runner :as sr]))
 
-(fact "we can read the spec lines"
-  (first (sr/spec-lines)) => "Normal Mode")
+(facts "about the avi spec runner"
+  (fact "we get the lines from the file"
+    (first (sr/spec-lines)) => "Normal Mode")
 
-(fact "we can find an arrange"
-  (sr/arrange) => "when editing \"hello world\"")
+  (fact "we can find an arrange"
+    (sr/arrange) => "when editing \"hello world\"")
 
-(facts "about the test parameters"
-  (fact "it has the keystrokes"
-    (:setup (sr/test-parameters)) => (contains {:after "$b"}))
+  (facts "about the test parameters"
+    (fact "it has the keystrokes"
+      (:setup (sr/test-parameters)) => (contains {:after "$b"}))
+    (fact "it has the resulting point"
+      (:point (sr/test-parameters)) => [0 6])))
 
-  (fact "it has the resulting point"
-    (:point (sr/test-parameters)) => [0 6]))
