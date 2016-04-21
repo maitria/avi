6da35a047dab94a2ec8e65f0320ce8b60473fec7
Jason Felice <jason.m.felice@gmail.com>
2016-04-20 21:43:22 -0400

    a single root pane fills the screen

---
+++ src/avi/panes.clj
@@ -0,0 +1,7 @@
+(ns avi.panes)
+
+(defn panes-to-render
+  [editor]
+  [{:lens 0
+    :offset [0 0]
+    :size [9 8]}])
---
+++ test/avi/t_panes.clj
@@ -0,0 +1,10 @@
+(ns avi.t-panes
+  (:require [midje.sweet :refer :all]
+            [avi.panes :as p]))
+
+(facts "about finding panes to render"
+  (fact "a single root pane fills the screen"
+    (p/panes-to-render {:viewport {:size [10 8]}
+                        :panes 0}) => [{:lens 0
+                                        :offset [0 0]
+                                        :size [9 8]}]))
