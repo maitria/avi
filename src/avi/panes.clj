637e1a23fd66b8004dd8a421ba902e3fecf34a62
Jason Felice <jason.m.felice@gmail.com>
2016-04-20 21:59:59 -0400

    a single horizonal split works correctly

--- src/avi/panes.clj
+++ src/avi/panes.clj
@@ -1,7 +1,18 @@
 (ns avi.panes)
 
+(defn- panes-to-render*
+  [[i j] [lines columns] tree]
+  (if (number? tree)
+    [{:lens tree
+      :offset [i j]
+      :size [lines columns]}]
+    (let [[_ lens-a offset-adjust lens-b] tree]
+      (concat
+        (panes-to-render* [i j] [offset-adjust columns] lens-a)
+        (panes-to-render* [(+ i offset-adjust) j]
+                          [(- lines offset-adjust) columns]
+                          lens-b)))))
+
 (defn panes-to-render
   [{:keys [panes] {[lines columns] :size} :viewport :as editor}]
-  [{:lens panes
-    :offset [0 0]
-    :size [(dec lines) columns]}])
+  (panes-to-render* [0 0] [(dec lines) columns] panes))
--- test/avi/t_panes.clj
+++ test/avi/t_panes.clj
@@ -11,4 +11,12 @@
     (p/panes-to-render {:viewport {:size [8 17]}
                         :panes 6}) => [{:lens 6
                                         :offset [0 0]
-                                        :size [7 17]}]))
+                                        :size [7 17]}])
+  (fact "a single horizonal split works correctly"
+    (p/panes-to-render {:viewport {:size [10 8]}
+                        :panes [:h 0 3 1]}) => [{:lens 0
+                                                 :offset [0 0]
+                                                 :size [3 8]}
+                                                {:lens 1
+                                                 :offset [3 0]
+                                                 :size [6 8]}]))
