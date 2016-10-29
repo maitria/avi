af891f04025551e01d4b1a8e09447bc79aa68de4
Jason Felice <jason.m.felice@gmail.com>
2016-10-29 17:38:45 -0400

    Partial specs for :avi.document/{undo,redo}-log

--- src/avi/document.clj
+++ src/avi/document.clj
@@ -4,4 +4,11 @@
 (s/def ::name string?)
 (s/def ::text string?)
 (s/def ::in-transaction? boolean?)
-(s/def ::document (s/keys :req [::name ::text]))
+(s/def ::undo-log (s/coll-of any? :into list?))
+(s/def ::redo-log (s/coll-of any? :into list?))
+
+(s/def ::document (s/keys :req [::name
+                                ::text
+                                ::in-transaction?
+                                ::undo-log
+                                ::redo-log]))
