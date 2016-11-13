6624c638778af393c7061fd2ebebbd4105917e41
Jason Felice <jason.m.felice@gmail.com>
2016-11-13 16:19:41 -0500

    Fix the docs for annotate

--- src/avi/xforms.clj
+++ src/avi/xforms.clj
@@ -5,7 +5,7 @@
 
   f accepts state and input and returns [state' input'].  To index inputs:
 
-     (annotate (fn [input n] [(assoc input ::pos n) (inc n)]) 0)"
+     (annotate (fn [n input] [(inc n) (assoc input ::pos n)]) 0)"
   [f init]
   (fn [rf]
     (let [state (volatile! init)]
