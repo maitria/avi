93ea11ba93c2f07dec58f007e0a64c659213869b
Jason Felice <jason.m.felice@gmail.com>
2016-11-24 12:54:20 -0500

    Upgrade midje

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
  :license {:name "avi license"
            :url "http://github.com/maitria/avi/README.md"}
  :jvm-opts ["-Djava.library.path=/usr/local/lib/avi"]
- :plugins [[lein-midje "3.0.0"]]
+ :plugins [[lein-midje "3.2.1"]]
  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                 [com.maitria/packthread "0.1.8"]
                 [org.clojars.oakes/parinfer "0.4.0"]
                 [potemkin "0.4.1"]]
- :profiles {:dev {:dependencies [[midje "1.9.0-alpha4"]
+ :profiles {:dev {:dependencies [[midje "1.9.0-alpha6"]
                                  [org.clojure/test.check "0.7.0"]
                                  [com.gfredericks/test.chuck "0.1.18"]]
                   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]}
